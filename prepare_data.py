162badf63bcdef160c9e719fd7abef857c06571e
Amir <amirhossein.nayebiastaneh@gmail.com>
2023-03-01 12:37:43 +0100

    Adds sequential data preparation.

--- prepare_data.py
+++ prepare_data.py
@@ -9,8 +9,8 @@ import yaml
 from moviepy.video.io.VideoFileClip import VideoFileClip
 import imageio
 import albumentations as A
-import shutil
 import cv2
+import data_stats
 
 data_file = "smart_plane.yaml"
 
@@ -19,18 +19,21 @@ def main(opt):
 
     print()
     video_files = []
-    total_augmentations = opt.total_augmentations
 
-    test_videos = opt.test_videos
+    test_videos = opt.test_videos  # Videos which will be used for creating test frames
+    train_size = opt.train_size
+    val_size = opt.val_size
+    test_size = opt.test_size
+    sequential = opt.sequential
 
-    if opt.source is not None:
+    if opt.source is not None:  # Source videos are provided, so the frames will be created in the 'image' folder.
         source = os.path.abspath(opt.source)
-        dir_source = os.path.isdir(source)
-        file_source = os.path.isfile(source)
-        if not dir_source and not file_source:
+        is_source_dir = os.path.isdir(source)
+        is_source_file = os.path.isfile(source)
+        if not is_source_dir and not is_source_file:
             sys.exit(f"There is no directory or file at path '{source}'!")
 
-        if dir_source:
+        if is_source_dir:
             all_files = os.listdir(source)
             video_files = [
                 fname for fname in all_files if fname.lower().endswith('.mp4')
@@ -42,12 +45,17 @@ def main(opt):
         else:
             video_files = [source]
 
+    # Create data directory if it does not exist.
+    # 'images' and 'labels' folders and train, val and test list files will be created in this folder.
     data_dir = os.path.abspath(opt.data_dir)
+
     if not os.path.isdir(data_dir):
         try:
             os.makedirs(data_dir)
         except Exception as e:
             sys.exit(f"Failed to create data directory '{data_dir}'\r\n{e}")
+
+    # Create images and labels directories if they do not exist.
     imgs_dir = os.path.join(data_dir, "images")
     lbs_dir = os.path.join(data_dir, 'labels')
     try:
@@ -58,29 +66,11 @@ def main(opt):
     except Exception as e:
         sys.exit(f"Failed to create output directories!\r\n{e}")
 
-    # more options here https://albumentations.ai/docs/getting_started/transforms_and_targets/
-    aug_transform = A.Compose([
-        A.RandomRain(p=0.5),
-        #  A.RandomSunFlare(p=0.5),
-        A.RandomFog(p=0.5, fog_coef_lower=0.01, fog_coef_upper=0.1),
-        # A.RandomBrightness(p=0.5),
-        # A.RGBShift(p=0.5),
-        #  A.RandomSnow(p=0.5),
-        # A.HorizontalFlip(p=0.5)), A.VerticalFlip(p=1),
-        # A.RandomContrast(limit = 0.5,p=0.5)),
-        # A.HueSaturationValue(p=1,hue_shift_limit=20, sat_shift_limit=30, val_shift_limit=50),
-        # A.Resize(width=1920, height=1080),
-        # A.RandomCrop(width=1280, height=720),
-        # A.Rotate(limit=40, p=0.9, border_mode=cv2.BORDER_CONSTANT),
-        # A.HorizontalFlip(p=0.5),
-        # A.VerticalFlip(p=0.1),
-        # A.RGBShift(r_shift_limit=25, g_shift_limit=25, b_shift_limit=25, p=0.9),
-        # A.OneOf([ A.Blur(blur_limit=3, p=0.5),A.ColorJitter(p=0.5),   ], p=1.0),
-    ])
+    annotated_videos = []  # List of annotated videos
 
     if len(video_files) > 0:
         for video_file in tqdm(video_files):
-            if dir_source:
+            if is_source_dir:
                 video_file_path = os.path.join(source, video_file)
                 annotation_file_path = os.path.join(
                     source,
@@ -95,6 +85,7 @@ def main(opt):
                 continue
 
             vidcap = VideoFileClip(video_file_path)
+
             # Get the duration of the video
             duration = vidcap.duration
 
@@ -102,10 +93,13 @@ def main(opt):
             frames_count = int(duration * vidcap.fps)
             frame_num_len = len(str(frames_count))
 
+            # Extract bounding boxes from annotation file
             boxesPerFrame, width, height = utility.AnnotationBox.GetBoxesFromXMLAnnotationFile(
                 annotation_file_path)
 
             videoFileBaseName = os.path.basename(video_file_path)
+            annotated_videos.append(videoFileBaseName)
+
             print(f'Retrieving frames from {videoFileBaseName}...')
 
             for frameNum, boxes in tqdm(boxesPerFrame.items()):
@@ -116,20 +110,8 @@ def main(opt):
 
                 if not os.path.isfile(frameFilePath):
                     image = vidcap.get_frame(frameNum / vidcap.fps)
-                    imageio.imwrite(frameFilePath, image)
-
-                    ################### Augmentation ###################
-                    augmentedFrameFilePathBase = os.path.splitext(
-                        frameFilePath)[0]
-                    for aug_index in tqdm(range(total_augmentations)):
-                        augmentations = aug_transform(image=image)
-                        augmented_img = augmentations["image"]
-                        augmentedFrameFilePath = augmentedFrameFilePathBase + "_" + str(
-                            aug_index) + ".png"
-                        cv2.imwrite(augmentedFrameFilePath, augmented_img)
-                        # imageio.imwrite(augmentedFrameFilePath, augmented_img)
-                    ####################################################
-
+                    cv2.imwrite(frameFilePath, image) # cv2 is much faster than imageio in writing image files
+                    # imageio.imwrite(frameFilePath, image) 
                 else:
                     print(f'{frameFilePath} exists. Skipping...')
 
@@ -152,15 +134,6 @@ def main(opt):
                     with open(labelFilePath, 'w') as file:
                         file.writelines(lines)
 
-                    ################### Augmentation ###################
-                    # Since we do not apply any geometric augmentation we simply copy
-                    # the generated label file for each augmented frame
-                    for aug_index in tqdm(range(total_augmentations)):
-                        augmentedLabelFilePath = labelFilePath.replace(
-                            ".txt", f"_{aug_index}.txt")
-                        shutil.copyfile(labelFilePath, augmentedLabelFilePath)
-                    ####################################################
-
     images = glob.glob(os.path.join(os.path.relpath(imgs_dir, '.'), "*.png"))
     # labels = glob.glob(os.path.join(os.path.relpath(lbs_dir, '.'), "*.txt"))
 
@@ -173,16 +146,42 @@ def main(opt):
 
     for i in range(len(images)):
         images[i] = "./images/" + os.path.basename(images[i])
-        # labels[i] = "./labels/" + os.path.basename(labels[i])
 
     if test_videos is None:
-        images_train, images_val_test, labels_train, labels_val_test = train_test_split(
-            images, images, train_size=opt.train_size)
+        if not sequential:
+            images_train, images_val_test = train_test_split(
+                images, train_size=train_size)
 
-        images_val, images_test, labels_val, labels_test = train_test_split(
-            images_val_test,
-            labels_val_test,
-            test_size=opt.test_size / (opt.test_size + opt.val_size))
+            images_val, images_test = train_test_split(images_val_test,
+                                                       test_size=test_size /
+                                                       (test_size + val_size))
+        else:
+            images_train = []
+            images_val = []
+            images_test = []
+
+            # Normalize train, val, and test sizes to make sure they sum up to one
+            sum_sizes = train_size + val_size + test_size
+            train_size /= sum_sizes
+            val_size /= sum_sizes
+            test_size /= sum_sizes
+
+            for annotated_video in annotated_videos:
+                all_video_frames = glob.glob(
+                    os.path.join(os.path.relpath(imgs_dir, '.'),
+                                 f"*{annotated_video}*"))
+                for i in range(len(all_video_frames)):
+                    all_video_frames[i] = "./images/" + os.path.basename(all_video_frames[i])
+
+                frames_count = len(all_video_frames)
+                train_index = 0
+                val_index = train_index + int(train_size * frames_count)
+                test_index = val_index + int(val_size * frames_count)
+
+                images_train.extend(all_video_frames[train_index:val_index])
+                images_val.extend(all_video_frames[val_index:test_index])
+                images_test.extend(all_video_frames[test_index:])
+
     else:
         images_test = []
         images_train_val = []
@@ -234,6 +233,7 @@ def main(opt):
 
     print(f"'{data_file}' created successfully in '{os.path.abspath('.')}'.\n")
 
+    data_stats.run()
 
 def parse_opt(known=False):
     parser = argparse.ArgumentParser(
@@ -272,11 +272,9 @@ def parse_opt(known=False):
         'List of test video files separated by spaces. If it is not provided, test data will be created randomly from all videos.',
         required=False)
 
-    parser.add_argument(
-        '--total_augmentations',
-        type=int,
-        help='Number of additional augmentations per frame. (Default = 0)',
-        default=0)
+    parser.add_argument('--sequential',
+                        action='store_true',
+                        help='Create datasets sequentially not randomly.')
 
     return parser.parse_known_args()[0] if known else parser.parse_args()
 
